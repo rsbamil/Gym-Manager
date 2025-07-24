490d154ffa25ff577ee0a4319240c96037ac022b
Rohit Kumar <rsbamil66@gamil.com>
2025-07-24 18:03:59 +0530

    Project Completed

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,14 +1,45 @@
 import React, { useState } from "react";
 import { ClipLoader } from "react-spinners";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const sendMail = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/send/mail",
+        {
+          name,
+          email,
+          message,
+        },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setName("");
+      setEmail("");
+      setMessage("");
+      toast.success(data.message);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.response.data.message);
+    }
+  };
   return (
     <section className="contact">
-      <form>
+      <form onSubmit={sendMail}>
         <h1>CONTACT US</h1>
         <div>
           <label>Name</label>
